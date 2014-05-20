ff509f4a49ce4ea464e9b5ee900a4308c4a886ad
Jon Craton <jncraton@gmail.com>
2014-05-20 18:08:56 -0400

    updated keyword test

---
+++ test.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+void main() {
+    printf ("Hello, world!");
+    
+    char dummy = '"';
+    char dummy2 = ("\"");
+    char dummy3 = ("'");
+    char dummy3 = ("\\\"");
+    
+    int this_is_false = 0;
+    
+    if (do_something || do_something_else) {
+    
+    }
+
+    if ((some_condition && 
+        another_condition ||
+        !some_other_condition)) {
+        do_something();
+    }
+    
+    do_something_else();
+
+    while (1) {
+        do_this_a_lot()
+    }
+}
